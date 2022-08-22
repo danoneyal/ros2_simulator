9d73180ff848c3969d0e20a3528bf1b9564af62c
Danon Eyal <asaf.anter@r-go.io>
2022-08-22 16:11:04 +0000

    wip

--- dev_ws/src/rgo_sim_ros2_pkg/src/rgo_sim_ros2_node.cpp
+++ dev_ws/src/rgo_sim_ros2_pkg/src/rgo_sim_ros2_node.cpp
@@ -26,6 +26,7 @@ class RgoRos2Node : public rclcpp::Node
       //create subscribers 
       subscriber_         = this->create_subscription<geometry_msgs::msg::Twist>("robot_sim_velocity", 1, std::bind(&RgoRos2Node::subscribe_message, this, _1));
       //odometry_subscriber_   = this->create_subscription<nav_msgs::msg::Odometry>("robot_sim_odometry", 1, std::bind(&RgoRos2Node::subscribe_odometry_message, this, _1));
+      //read odom from robot
       odometry_subscriber_   = this->create_subscription<nav_msgs::msg::Odometry>("odom", 1, std::bind(&RgoRos2Node::subscribe_odometry_message, this, _1));
       
       
